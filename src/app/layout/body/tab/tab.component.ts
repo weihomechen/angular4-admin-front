c90c422747a6e108d64d5ad417172b8eb72086d7
weihome <weihome_chen@163.com>
2017-05-11 12:04:35 +0800

    添加mdl样式

---
+++ src/app/layout/body/tab/tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.css']
+})
+export class TabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
