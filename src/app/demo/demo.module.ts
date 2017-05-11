193b2ee08c87323926b2c97f76182c7a2d091950
weihome <weihome_chen@163.com>
2017-05-11 18:47:30 +0800

    项目初始化

---
+++ src/app/demo/demo.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoComponent } from './demo/demo.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DemoComponent]
+})
+export class DemoModule { }
