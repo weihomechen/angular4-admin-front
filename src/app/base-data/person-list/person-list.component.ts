ff6cce9ece5ad879e2f1c6217c14b0bec31f6e2a
weihome <weihome_chen@163.com>
2017-05-24 17:52:43 +0800

    人员模块完善

--- src/app/base-data/person-list/person-list.component.ts
+++ src/app/base-data/person-list/person-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { MdlDefaultTableModel, IMdlTableModelItem } from '@angular-mdl/core';
 import { ITablePersonItem } from '../model/table-person-item';
@@ -8,21 +9,18 @@ import { TabControlService } from '../../layout/header/tab/tabControl.service';
 import { PersonManageService } from '../person-manage.service';
 import 'rxjs/add/operator/toPromise';
 
-declare var $: any;
+declare var SweetAlert: any;
 
 @Component({
     selector: 'app-person-list',
     templateUrl: './person-list.component.html',
     styleUrls: ['./person-list.component.css']
 })
-export class PersonListComponent implements OnInit, AfterViewInit {
+export class PersonListComponent implements OnInit {
 
-    text1: string;
-    text2: string;
-    text3: string;
     tableData;
+    selected;
     tableModel = new MdlDefaultTableModel([
-        { key: 'id', name: 'id', sortable: true },
         { key: 'name', name: 'name', sortable: true, numeric: true },
         { key: 'sex', name: 'sex', numeric: true },
         { key: 'team', name: 'team', sortable: true },
@@ -30,6 +28,12 @@ export class PersonListComponent implements OnInit, AfterViewInit {
         { key: 'role', name: 'role', numeric: true },
         { key: 'position', name: 'position', sortable: true, numeric: true }
     ]);
+    searchObj = {
+        name: '',
+        team: '',
+        account: ''
+    };
+
     constructor(private http: Http,
         private tabControlService: TabControlService,
         private personManageService: PersonManageService,
@@ -37,32 +41,83 @@ export class PersonListComponent implements OnInit, AfterViewInit {
     }
 
     ngOnInit() {
-        this.personManageService.getPersons().then(personArr => {
+        this.initTable();
+    }
+
+    initTable(searchObj?) {
+        this.personManageService.getPersons(searchObj).then(personArr => {
             this.tableData = personArr;
+            this.tableModel.data.length = 0;
             this.tableModel.addAll(this.tableData);
         }
         );
     }
 
-    ngAfterViewInit() {
-        const tableNode = $('#person_manage_table');
-        const trNode = $('tr', tableNode);
-        $('tr td:th-child(2)', tableNode).css('display', 'none');
+    selectionChanged($event) {
+        this.selected = $event.value;
     }
 
+    // 添加人员
     addPerson() {
         this.tabControlService.newTab({ 'name': '添加人员', 'link': '/base-data/person-add' });
         this.router.navigateByUrl('base-data/person-add');
     }
 
-    onInputBlur(event) {
+    // 查看人员
+    checkPerson() {
+        if (!this.selected || this.selected.length !== 1) {
+            SweetAlert.notice('请选择一个人员进行查看', '查看人员');
+            return;
+        }
+    }
 
+    // 修改人员
+    editPerson() {
+        if (!this.selected || this.selected.length !== 1) {
+            SweetAlert.notice('请选择一个人员进行修改', '修改人员');
+            return;
+        }
+    }
+
+    // 删除人员
+    delPerson() {
+        if (!this.selected || this.selected.length === 0) {
+            SweetAlert.notice('请选择人员进行删除', '删除人员');
+            return;
+        } else {
+            SweetAlert.confirm('确定删除这' + this.selected.length + '个人员？', '删除人员', function () {
+
+            });
+        }
+    }
+
+    // 搜索
+    search() {
+        if (!this.searchObj.name && !this.searchObj.team && !this.searchObj.account) {
+            SweetAlert.notice('请至少输入一个搜索条件', '搜索人员');
+            return;
+        } else {
+            // console.log('searchObj:' + JSON.stringify(this.searchObj));
+            this.initTable(this.searchObj);
+        }
+    }
+
+    // 重置
+    reset() {
+        this.searchObj = {
+            name: '',
+            team: '',
+            account: ''
+        };
+        this.initTable();
+    }
+
+    onInputBlur(event) {
     }
 
     onInputFocus(event) {
     }
 
     onInputKeyup(event) { }
-
 
 }
--- src/app/base-data/person-manage.service.ts
+++ src/app/base-data/person-manage.service.ts
@@ -9,12 +9,17 @@ export class PersonManageService {
     private personUrl = 'assets/mock-data/person.json';
     constructor(private http: Http) { }
 
-    getPersons(): Promise<ITablePersonItem[]> {
+    // 获取人员，可带搜索条件
+    getPersons(searchObj?): Promise<ITablePersonItem[]> {
         return this.http
             .get(this.personUrl)
             .toPromise()
             .then(response => response.json() as ITablePersonItem[])
             .catch(this.handleError);
+    }
+
+    addPerson(personObj){
+        
     }
 
     private handleError(error: any): Promise<any> {
